313abe5b997a517b1a821e9b1d06dd28a6dbc3c5
Anshu Jee <jeeanshu24@gmail.com>
2021-07-13 19:33:18 +0530

    Test 3

--- Method_overriding1.java
+++ Method_overriding1.java
@@ -9,14 +9,14 @@ class Hdfc_bank extends Rbi
 {
 	void rateofintrest()
 	{
-		System.out.println("Rate of Intrest in HDFC Bank on Personal Loan is 10%");
+		System.out.println("Rate of Intrest in HDFC Bank on Personal Loan is 7%");
 	}
 }
 class Icici_bank extends Rbi
 {
 	void rateofintrest()
 	{
-		System.out.println("Rate of Intrest in Icici Bank on Personal Loan is 8%");
+		System.out.println("Rate of Intrest in Icici Bank on Personal Loan is 6%");
 	}
 }
 class Method_overriding1
